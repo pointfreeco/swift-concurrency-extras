1f2ff32c5d48c04bdc6dbc9c40969cdc37dfb6f9
Stephen Celis <stephen@stephencelis.com>
2024-08-13 10:16:13 -0700

    wip

--- Sources/ConcurrencyExtras/Mutex.swift
+++ Sources/ConcurrencyExtras/Mutex.swift
@@ -4,7 +4,6 @@
   /// A synchronization primitive that protects shared mutable state via mutual exclusion.
   ///
   /// A back-port of Swift's `Mutex` type for wider platform availability.
-  @frozen
   @_staticExclusiveOnly
   @available(iOS, obsoleted: 18, message: "Use 'Synchronization.Mutex', instead.")
   @available(macOS, obsoleted: 15, message: "Use 'Synchronization.Mutex', instead.")
@@ -12,25 +11,18 @@
   @available(visionOS, obsoleted: 2, message: "Use 'Synchronization.Mutex', instead.")
   @available(watchOS, obsoleted: 11, message: "Use 'Synchronization.Mutex', instead.")
   public struct Mutex<Value: ~Copyable>: ~Copyable {
-    @usableFromInline
-    let lock = NSLock()
-
-    @usableFromInline
-    let box: Box
+    private let lock = NSLock()
+    private let box: Box
 
     /// Initializes a value of this mutex with the given initial state.
     ///
     /// - Parameter initialValue: The initial value to give to the mutex.
-    @_transparent
     public init(_ initialValue: consuming sending Value) {
       box = Box(initialValue)
     }
 
-    @usableFromInline
-    final class Box {
-      @usableFromInline
+    private final class Box {
       var value: Value
-      @usableFromInline
       init(_ initialValue: consuming sending Value) {
         value = initialValue
       }
@@ -41,7 +33,6 @@
 
   extension Mutex where Value: ~Copyable {
     /// Calls the given closure after acquiring the lock and then releases ownership.
-    @_transparent
     public borrowing func withLock<Result: ~Copyable, E: Error>(
       _ body: (inout sending Value) throws(E) -> sending Result
     ) throws(E) -> sending Result {
@@ -51,7 +42,6 @@
     }
 
     /// Attempts to acquire the lock and then calls the given closure if successful.
-    @_transparent
     public borrowing func withLockIfAvailable<Result: ~Copyable, E: Error>(
       _ body: (inout sending Value) throws(E) -> sending Result
     ) throws(E) -> sending Result? {
@@ -62,17 +52,14 @@
   }
 
   extension Mutex where Value == Void {
-    @_transparent
     public borrowing func _unsafeLock() {
       lock.lock()
     }
 
-    @_transparent
     public borrowing func _unsafeTryLock() -> Bool {
       lock.try()
     }
 
-    @_transparent
     public borrowing func _unsafeUnlock() {
       lock.unlock()
     }
