4708c9ff49cd7ab1ae7c110d15e32c254f427f89
Sven A. Schmidt <sas@finestructure.co>
2024-12-09 20:44:49 +0100

    Constrain change to compiler(>=6)

--- Sources/ConcurrencyExtras/Result.swift
+++ Sources/ConcurrencyExtras/Result.swift
@@ -1,3 +1,4 @@
+#if compiler(>=6)
 extension Result {
   /// Creates a new result by evaluating an async throwing closure, capturing the returned value as
   /// a success, or any thrown error as a failure.
@@ -12,3 +13,19 @@ extension Result {
     }
   }
 }
+#else
+extension Result where Failure == Swift.Error {
+  /// Creates a new result by evaluating an async throwing closure, capturing the returned value as
+  /// a success, or any thrown error as a failure.
+  ///
+  /// - Parameter body: A throwing closure to evaluate.
+  @_transparent
+  public init(catching body: () async throws -> Success) async {
+    do {
+      self = .success(try await body())
+    } catch {
+      self = .failure(error)
+    }
+  }
+}
+#endif
