f5d0abc902f610c15795047febb65100f973d13c
Sven A. Schmidt <sas@finestructure.co>
2024-12-16 21:45:48 +0100

    Support typed throws (#52)
    * Support typed throws
    
    * Update Sources/ConcurrencyExtras/Result.swift
    
    Co-authored-by: Stephen Celis <stephen.celis@gmail.com>
    
    * Constrain change to compiler(>=6)
    
    * Also gate the test to Swift 6
    
    * Include func g in ifdef
    
    * wip
    
    ---------
    
    Co-authored-by: Stephen Celis <stephen.celis@gmail.com>
    Co-authored-by: Stephen Celis <stephen@stephencelis.com>

---
+++ Tests/ConcurrencyExtrasTests/ResultTests.swift
@@ -0,0 +1,62 @@
+import ConcurrencyExtras
+import XCTest
+
+final class ResultTests: XCTestCase {
+  struct SomeError: Error, Equatable { }
+  func f(_ value: Int) async throws -> Int {
+    if value == 42 {
+      return 42
+    } else {
+      throw SomeError()
+    }
+  }
+
+  func testBasics() async {
+    do {
+      let res = await Result { try await f(42) }
+      XCTAssertEqual(try res.get(), 42)
+    }
+    do {
+      let res = await Result { try await f(0) }
+      do {
+        _ = try res.get()
+      } catch let error as SomeError {
+        XCTAssertEqual(error, SomeError())
+      } catch {
+        XCTFail("unexpected error: \(error)")
+      }
+    }
+  }
+
+  #if compiler(>=6)
+    func g(_ value: Int) async throws(SomeError) -> Int {
+      if value == 42 {
+        return 42
+      } else {
+        throw SomeError()
+      }
+    }
+
+    func testTypedThrows() async {
+      let res = await Result { () async throws(SomeError) -> Int in try await g(0) }
+      do {
+        _ = try res.get()
+      } catch {
+        XCTAssertEqual(error, SomeError())
+      }
+    }
+
+    func h() async throws(SomeError) -> Int {
+      throw SomeError()
+    }
+
+    func testTypedThrowsInference() async {
+      let res = await Result(catching: h)
+      do {
+        _ = try res.get()
+      } catch {
+        XCTAssertEqual(error, SomeError())
+      }
+    }
+  #endif
+}
